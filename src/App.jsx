d83124482b3d682fec542841b7190d0459190c69
Simone Luzi <simone.luzi@studio.unibo.it>
2025-04-25 01:25:57 +0200

    primi 3 blocchi fatti

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,63 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import foto from './assets/foto.jpeg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
   return (
-    <div className="App">
-      <div className="container" style={{ marginBottom: "2rem"}}>
-        <p>È con grandissima gioia che</p>
-        <h1 style={{fontFamily: "Great Vibes"}}>Nicola e Lorena</h1>
-        <p>e le loro famiglie</p>
-        <p>vi invitano al loro matrimonio</p>
-        <p style={{fontFamily: 'Satisfy'}}>
+    <div>
+      {/* Primo blocco invito */}
+      <div className="container">
+        <p className="sottile">È con grandissima gioia che</p>
+        <h1 className="titolo">Nicola e Lorena</h1>
+        <p className="sottile">e le loro famiglie</p>
+        <p className="sottile">vi invitano al loro matrimonio</p>
+        <p className="data">
           4 settembre 2025
-          <br/>
+          <br />
           ore 16:00
         </p>
-        <p style={{ marginBottom: "2rem", fontFamily: 'Playfair Display'}}>
-          <a href="https://maps.app.goo.gl/brCmg2hXxM8nvtnZ7" target="_blank" rel="noopener noreferrer">
+        <p className="location">
+          <a
+            href="https://maps.app.goo.gl/brCmg2hXxM8nvtnZ7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Villa Vittoria
-            <br/>
-            Via Cifelli, 44 - Boscotrecase (Na)
+            <br />
+            Via Cifelli 44 - Boscotrecase (Na)
           </a>
         </p>
       </div>
-      <img src={foto} alt="Proposta di matrimonio di Nicola e Lorena" width="300" />
+
+      {/* Secondo blocco call-to-action */}
+      <div className="cta-layout">
+        <div className="cta-text">
+          <h2>Il grande giorno</h2>
+          <p>
+            Siete pronti a vivere questo grande giorno insieme? Noi sì!<br /><br />
+            Abbiamo bisogno della vostra partecipazione per renderlo ancora più speciale.<br /><br />
+            Fateci sapere al più presto se sarete con noi!<br /><br />
+            Non vediamo l'ora di condividere questa esperienza con voi!
+          </p>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLScmrbUrSAhwl3G0CWdRvLfsQzhc8cZGi8hU9KO9FOq7A6g_jg/viewform?usp=header"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="cta-button"
+          >
+            Conferma la tua presenza
+          </a>
+        </div>
+        <div className="cta-img-container">
+          <img
+            src={foto}
+            alt="Nicola e Lorena"
+            className="cta-img"
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
